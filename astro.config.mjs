ad6a6e56768fb79054888dfce50b82c6014bd44b
Teddy563 <tediteditu511@gmail.com>
2024-03-03 15:20:32 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,7 +22,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://563.github.com',
+  site: 'https://563.vercel.app',
   base: "/",
   integrations: [
     tailwind(),
