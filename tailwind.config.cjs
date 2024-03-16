3ed8d962434244d2a32ab5e6d6a3d5c0858b5c30
Teddy563 <tediteditu511@gmail.com>
2024-03-17 00:19:57 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Montserrat", "sans-serif", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
       },
     },
   },
