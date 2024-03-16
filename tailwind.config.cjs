ebd7d517bb16bc9ec06d9d90819d9229cbe82ef8
Teddy563 <tediteditu511@gmail.com>
2024-03-16 22:27:14 +0200

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme")
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  }
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class", // allows toggling dark mode manually
   theme: {
